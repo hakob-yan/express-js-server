a4a3da136b0b7bc202e2a8603f47930f5e42549e
hakob-yan <devhakob@gmail.com>
2026-06-10 01:01:22 +0400

    new data

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, env.HOST, () =>
-  console.log(`Server is running on port ${env.PORT || env.HOST}`),
+app.listen(env.PORT, () =>
+  console.log(`Server is running on port ${env.PORT}`),
 );
 
