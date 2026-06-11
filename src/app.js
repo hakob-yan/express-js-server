bbe0a07c63aadd8b1c829d3fc252d48ba091e2e8
hakob-yan <devhakob@gmail.com>
2026-06-11 19:25:53 +0400

    healt

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import userRoutes from "./routes/user.route.js";
+import healthRoute from "./routes/healts.route.js";
 
 import ErrorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
 
@@ -9,6 +10,8 @@ app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/health", healthRoute);
+
 app.use(ErrorHandlerMiddleware.handleErrors);
 
 export default app;
